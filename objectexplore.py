379300b6a31935427f4d3d24107986b15456c9d0
aurelpere <aurel.pere@gmail.com>
2022-03-19 11:58:43 +0100

    added continuous integration and lint

--- objectexplore.py
+++ objectexplore.py
@@ -1,20 +1,50 @@
+#!/usr/bin/python3
+# coding: utf-8
+"""
+this is objectexplore.py module
+"""
+import types
+import builtins
+
 def diro(obj):
-    callables=['{}{}'.format(str(attr),'()') for attr in dir(obj) if callable(getattr(obj, attr))]
+    "return callables and attributes of the object argument"
+    callables = [
+        f'{attr}()' for attr in dir(obj) if callable(getattr(obj, attr))
+    ]
     print('callables')
     print(callables)
     print('\n')
     print('attributes')
-    attributes=['{}'.format(str(attr)) for attr in dir(obj) if not callable(getattr(obj, attr))]
+    attributes = [
+        f'{attr}' for attr in dir(obj) if not callable(getattr(obj, attr))
+    ]
     print(attributes)
-    
+    return 1
 
 def dirdoc(obj):
+    "return docs of callables of the object argument"
     for attr in dir(obj):
-         if callable(getattr(obj, attr)):
-             print('{}{}'.format(str(attr),'()'))
-             if attr.__doc__:
-                 print(attr.__doc__)
-             print('\n\n')
-             
+        if callable(getattr(obj, attr)):
+            print(f'{attr}()')
+            if attr.__doc__:
+                print(attr.__doc__)
+            print('\n\n')
+    return 1
+
 def dirpath(obj):
-    print(obj.__globals__['__file__'])
+    "return path of file wher the code of of the object argument is written"
+    try:
+        print(obj.__globals__['__file__'])
+        print(type(obj))
+    except:
+        try:
+            print(type(__builtins__))
+            if isinstance(obj, types.BuiltinFunctionType) or obj in builtins.__dict__.values():
+                print('builtin object,class or function: cf shorturl.at/bhtA2 (stackoverflow)')
+                print(type(obj))
+                return 1
+            print(f'le code se trouve dans le module {obj.__module__}')
+            print(type(obj))
+        except:
+            print('chemin non trouvé')
+    return 1
---
+++ test_objectexplore.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+# coding: utf-8
+"""
+this is test_objectexplore.py
+"""
+import sys
+import io
+import numpy
+import pandas as pd
+from objectexplore import diro
+from objectexplore import dirdoc
+from objectexplore import dirpath
+
+def test_diro():
+    """test diro"""
+    string_object = ''
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    diro(string_object)
+    sys.stdout = sys.__stdout__
+    for attr in dir(string_object):
+        if callable(getattr(string_object, attr)):
+            assert f'{attr}()' in captured_output.getvalue()
+        else:
+            assert str(attr) in captured_output.getvalue()
+    return 1
+
+def test_dirdoc():
+    """test dirdoc"""
+    string_object = ''
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    dirdoc(string_object)
+    sys.stdout = sys.__stdout__
+    for attr in dir(string_object):
+        if callable(getattr(string_object, attr)):
+            assert f'{attr}()' in captured_output.getvalue()
+            assert str(attr.__doc__) in captured_output.getvalue()
+    return 1
+
+def test_dirpath():
+    """test dirpath"""
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    dirpath(str)
+    sys.stdout = sys.__stdout__
+    print(captured_output.getvalue())
+    assert "check shorturl.at/bhtA2" in captured_output.getvalue()
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    dirpath(numpy.where)
+    sys.stdout = sys.__stdout__
+    assert "site-packages/numpy/" in captured_output.getvalue()
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    dirpath(pd.DataFrame)
+    sys.stdout = sys.__stdout__
+    assert "module pandas.core.frame" in captured_output.getvalue()
+    captured_output = io.StringIO()
+    sys.stdout = captured_output
+    dirpath(numpy.tan)
+    sys.stdout = sys.__stdout__
+    assert "chemin non trouvé" in captured_output.getvalue()
+    return 1
